ab8b017c7150fc2fbdf2349a51d460f878dace32
dat17-05-ishiduki <175b005gp12@gmail.com>
2019-05-31 12:45:20 +0900

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <p>@{{ message }}</p>
+    <button v-on:click="messageChange('PHPのフレームワークといえばLaravel！')">「PHP」が好き</button>
+    <button v-on:click="messageChange('RubyのフレームワークといえばRails！')">「Ruby」が好き</button>
+    <button v-on:click="messageChange('JavaScriptのライブラリといえばVue.js！')">「JavaScript」が好き</button>
+  </div>
+</template>
+<script>
+export default {
+  el: "#app",
+  data() {
+    return { message: "好きな言語は？" };
+  },
+  methods: {
+    messageChange: function(lang) {
+      this.message = lang;
+    }
+  }
+};
+</script>
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
