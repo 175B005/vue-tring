2bc826f0a5abbe69305fe03f8429ab0b79ef27e1
dat17-05-ishiduki <175b005gp12@gmail.com>
2019-05-31 14:46:11 +0900

    カウント追加
    同じものの分岐とカウントの追加

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,36 @@
 <template>
   <div id="app">
     https://laraweb.net/surrounding/4467/
-    <p>@{{ message }}</p>
+    <p>{{ message }}</p>
+    <p>{{ count }}</p>
     
     <button v-on:click="messageChange('PHPのフレームワークといえばLaravel！')">「PHP」が好き</button>
-    <button v-on:click="messageChange('RubyのフレームワークといえばRails！')">「Ruby」が好き</button>
     <button v-on:click="messageChange('JavaScriptのライブラリといえばVue.js！')">「JavaScript」が好き</button>
+    </br>
+    <button v-on:click="countChange(1)">カウントを追加</button>
   </div>
 </template>
 <script>
 export default {
   el: "#app",
   data() {
-    return { message: "好きな言語は？" };
+    return {
+       message: "好きな言語は？",
+       count: 0,
+    };
   },
   methods: {
     messageChange: function(lang) {
-      this.message = lang;
+      if(this.message !== lang){
+        this.message = lang;
+        this.countChange(-this.count);
+      }
+      else{
+        this.countChange(1);
+      }
+    },
+    countChange: function(add) {
+      this.count += add;
     }
   }
 };
